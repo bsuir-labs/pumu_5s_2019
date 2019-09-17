41fc3de57e8bd617f8677b5de435f716ee99669c
Kanstancin Novikau <k.novikov@rtp.by>
2019-09-17 10:32:04 +0300

    Code style fixed
    replaced tabs with spaces

--- labs/1/source.c
+++ labs/1/source.c
@@ -10,25 +10,25 @@ void Delay(volatile uint32_t tick)
 
 int main(void)
 {
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-	GPIO_InitTypeDef GPIO_InitStruct;
-	GPIO_StructInit(&GPIO_InitStruct);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_StructInit(&GPIO_InitStruct);
 
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_14;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD, &GPIO_InitStruct);
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_14;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOD, &GPIO_InitStruct);
 
 
     while(1)
     {
-    	GPIO_SetBits(GPIOD, GPIO_Pin_14);
-		Delay(500000);
-		GPIO_ResetBits(GPIOD, GPIO_Pin_14);
-		GPIO_SetBits(GPIOD, GPIO_Pin_12);
-		Delay(500000);
-		GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+        GPIO_SetBits(GPIOD, GPIO_Pin_14);
+        Delay(500000);
+        GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+        GPIO_SetBits(GPIOD, GPIO_Pin_12);
+        Delay(500000);
+        GPIO_ResetBits(GPIOD, GPIO_Pin_12);
     }
 }
--- labs/2/src/source-main.c
+++ labs/2/src/source-main.c
@@ -9,71 +9,71 @@ unsigned short interruptState = 0;
 
 void EXTI0_IRQHandler(void)
 {
-	if(EXTI_GetITStatus(EXTI_Line0) != RESET)
-	{
-		interruptState = 1;
-		EXTI_ClearITPendingBit(EXTI_Line0);
-	}
+    if(EXTI_GetITStatus(EXTI_Line0) != RESET)
+    {
+        interruptState = 1;
+        EXTI_ClearITPendingBit(EXTI_Line0);
+    }
 }
 
 void Delay(int ticks)
 {
-	for (int i = 0; i < ticks; ++i);
+    for (int i = 0; i < ticks; ++i);
 }
 
 int main()
 {
-	// initializing LED lights
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-	GPIO_InitTypeDef GPIO_InitStruct;
-	GPIO_StructInit(&GPIO_InitStruct);
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD, &GPIO_InitStruct);
+    // initializing LED lights
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_StructInit(&GPIO_InitStruct);
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOD, &GPIO_InitStruct);
 
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-	GPIO_InitTypeDef GPIO_ButtonStruct;
-	GPIO_StructInit(&GPIO_ButtonStruct);
-	GPIO_ButtonStruct.GPIO_Pin = GPIO_Pin_0;
-	GPIO_ButtonStruct.GPIO_Mode = GPIO_Mode_IN;
-	GPIO_ButtonStruct.GPIO_Speed = GPIO_Speed_2MHz;
-	GPIO_ButtonStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_ButtonStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD, &GPIO_InitStruct);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+    GPIO_InitTypeDef GPIO_ButtonStruct;
+    GPIO_StructInit(&GPIO_ButtonStruct);
+    GPIO_ButtonStruct.GPIO_Pin = GPIO_Pin_0;
+    GPIO_ButtonStruct.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_ButtonStruct.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_ButtonStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_ButtonStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOD, &GPIO_InitStruct);
 
-	RCC_AHB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
-	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
-	EXTI_InitTypeDef EXTI_InitStruct;
-	EXTI_StructInit(&EXTI_InitStruct);
-	EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
-	EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising;
-	EXTI_InitStruct.EXTI_Line = EXTI_Line0;
-	EXTI_InitStruct.EXTI_LineCmd = ENABLE;
-	EXTI_Init(&EXTI_InitStruct);
+    RCC_AHB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+    EXTI_InitTypeDef EXTI_InitStruct;
+    EXTI_StructInit(&EXTI_InitStruct);
+    EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising;
+    EXTI_InitStruct.EXTI_Line = EXTI_Line0;
+    EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStruct);
 
-	NVIC_InitTypeDef NVIC_InitStruct;
-	NVIC_InitStruct.NVIC_IRQChannel = EXTI0_IRQn;
-	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x01;
-	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x01;
-	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStruct);
+    NVIC_InitTypeDef NVIC_InitStruct;
+    NVIC_InitStruct.NVIC_IRQChannel = EXTI0_IRQn;
+    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x01;
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x01;
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStruct);
 
-	while (1)
-	{
-		if (interruptState == 1)
-		{
-			// interrupted
-			Delay(60000);
-			if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 1) 
-			{
-				GPIO_ToggleBits(GPIOD, GPIO_Pin_13);
-			}
-			interruptState = 0;
-		}
-	}
+    while (1)
+    {
+        if (interruptState == 1)
+        {
+            // interrupted
+            Delay(60000);
+            if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 1) 
+            {
+                GPIO_ToggleBits(GPIOD, GPIO_Pin_13);
+            }
+            interruptState = 0;
+        }
+    }
 
-	return 0;
+    return 0;
 }
